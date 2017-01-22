57633dd87cc4653148de1719941b702a74b18e60
ranamby7 <nambinintsoa.razafindrafito@gmail.com>
2017-01-22 17:09:02 +0300

    modification de index.php

--- index.php
+++ index.php
@@ -1,11 +1,3 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>
-			Test Php app
-		</title>
-	</head>
-	<body>
-		Le test sur le php app marche très bien.
-	</body>
-</html>
+<?php
+	include("index.html");
+?>
