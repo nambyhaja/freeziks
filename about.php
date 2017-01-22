69332524a539a9a44acb4804f50894a236d0345e
ranamby7 <nambinintsoa.razafindrafito@gmail.com>
2017-01-22 12:32:47 +0300

    ajout de about.php et index.php

---
+++ about.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>
+			Test Php app
+		</title>
+	</head>
+	<body>
+		Ceci est un test si le fichier about.php marche.
+	</body>
+</html>
---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>
+			Test Php app
+		</title>
+	</head>
+	<body>
+		Le test sur le php app marche très bien.
+	</body>
+</html>
